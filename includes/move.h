bf9f2a671531df35f6b7c8683899a9f4ea20a8ca
Thomas Byr <tbouyer2000@gmail.com>
2022-03-09 15:31:56 +0100

    an enum class

---
+++ includes/move.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "lib.h"
+#include "position.h"
+
+class Move {
+  public:
+    static const int QueenSideCastle = 0;
+    static const int KingSideCastle = 1;
+    static const int PieceMove = 2;
+    static const int Resign = 3;
+    static const int Invalid = 4;
+
+    Move();
+    ~Move();
+
+    int from_string(const std::string &move_string);
+
+    friend std::ostream &operator<<(std::ostream &os, const Move &move);
+
+  private:
+    Position from;
+    Position to;
+    int move_type;
+};
---
+++ src/move.cpp
@@ -0,0 +1,66 @@
+#include "move.h"
+
+Move::Move() {
+    from = Position();
+    to = Position();
+}
+
+int Move::from_string(const std::string &move_string) {
+    if (move_string == "resign" || move_string == "resigns") {
+        return Resign;
+    } else if (move_string == "queenside castle" ||
+               move_string == "castle queenside" || move_string == "O-O-O" ||
+               move_string == "0-0-0" || move_string == "o-o-o") {
+        move_type = QueenSideCastle;
+        return QueenSideCastle;
+    } else if (move_string == "kingside castle" ||
+               move_string == "castle kingside" || move_string == "O-O" ||
+               move_string == "0-0" || move_string == "o-o") {
+        move_type = KingSideCastle;
+        return KingSideCastle;
+    } else {
+        std::vector<std::string> move_parts;
+        std::string delim = " ";
+        size_t last = 0, next = 0;
+        while ((next = move_string.find(delim, last)) != std::string::npos) {
+            move_parts.push_back(move_string.substr(last, next - last));
+            last = next + delim.length();
+        }
+        move_parts.push_back(move_string.substr(last));
+
+        if (move_parts.size() == 1 && move_parts[0].size() == 4) {
+            from = Position(move_parts[0].substr(0, 2));
+            to = Position(move_parts[0].substr(2, 2));
+            move_type = PieceMove;
+            return PieceMove;
+        } else if (move_parts.size() == 2) {
+            from = Position(move_parts[0]);
+            to = Position(move_parts[1]);
+            move_type = PieceMove;
+            return PieceMove;
+        } else if (move_parts.size() == 3 && move_parts[1] == "to") {
+            from = Position(move_parts[0]);
+            to = Position(move_parts[2]);
+            move_type = PieceMove;
+            return PieceMove;
+        } else {
+            move_type = Invalid;
+            return Invalid;
+        }
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const Move &move) {
+    switch (move.move_type) {
+    case Move::PieceMove:
+        return os << move.from << " to " << move.to;
+    case Move::KingSideCastle:
+        return os << "O-O";
+    case Move::QueenSideCastle:
+        return os << "O-O-O";
+    case Move::Resign:
+        return os << "Resign";
+    default:
+        return os << "Invalid";
+    }
+}

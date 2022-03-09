c974995cfbdd370b4031c0b8fe87f20f63719614
Thomas Byr <tbouyer2000@gmail.com>
2022-03-09 19:29:08 +0100

    this is to avoid circular includes

---
+++ includes/check.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <vector>
+
+#include "board.h"
+#include "move.h"
+#include "piece.h"
+#include "position.h"
+
+std::vector<Move> get_legal_moves(const Piece &piece, const Board &board);
+
+bool is_legal_move(const Piece &piece, const Position &new_pos,
+                   const Board &board);
+
+bool is_legal_attack(const Piece &piece, const Position &new_pos,
+                     const Board &board);
---
+++ src/check.cpp
@@ -0,0 +1,9 @@
+#include "check.h"
+
+std::vector<Move> get_legal_moves(const Piece &piece, const Board &board) {}
+
+bool is_legal_move(const Piece &piece, const Position &new_pos,
+                   const Board &board) {}
+
+bool is_legal_attack(const Piece &piece, const Position &new_pos,
+                     const Board &board) {}

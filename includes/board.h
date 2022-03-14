780905aef907772945ac1feb52ed1dd0c3c1ec03
Thomas Byr <tbouyer2000@gmail.com>
2022-03-14 15:00:31 +0100

    board builder

--- includes/board.h
+++ includes/board.h
@@ -2,6 +2,7 @@
 
 #include "lib.h"
 #include "move.h"
+#include "piece.h"
 #include "position.h"
 #include "square.h"
 
@@ -31,22 +32,67 @@ class Board {
     Board();
     ~Board();
 
-    double value_for(Color ally_color) const;
+    double value_for(const Color &ally_color) const;
     Color get_current_player_color() const;
-    Board apply_eval_move(const Move &move) const;
-    std::vector<Move> get_legal_moves() const;
+
+    Board apply_eval_move(const Move &move) const; // todo
+    std::vector<Move> get_legal_moves() const;     // todo
 
     std::tuple<Move, unsigned, double> get_next_best_move(int depth) const;
     std::tuple<Move, unsigned, double> get_next_worst_move(int depth) const;
     double minimax(int depth, double alpha, double beta, bool is_maximizing,
                    Color getting_move_for, unsigned board_count) const;
 
+    CastlingRights *black_castling_rights;
+    CastlingRights *white_castling_rights;
+
+    Square get_square(const Position &pos) const;
+    void add_piece(Piece &piece);
+    Piece *get_piece(const Position &pos);
+
+    bool has_ally_piece(const Position &pos, const Color &ally_color);
+    bool has_enemy_piece(const Position &pos, const Color &enemy_color);
+    bool has_piece(const Position &pos);
+    bool has_no_piece(const Position &pos);
+
+    Position get_king_position(const Color &color) const;
+
+    bool is_threatened(const Position &pos, const Color &ally_color) const;
+    bool is_in_check(const Color &color) const;
+
+    Board move_piece(const Position &from, const Position &to) const;
+
+    bool can_kingside_castle(const Color &color) const;
+    bool can_queenside_castle(const Color &color) const;
+
+    bool has_sufficient_material(const Color &color) const;
+    bool has_insufficient_material(const Color &color) const;
+
+    bool is_stalemate() const;
+    bool is_checkmate() const;
+
+    Board change_turn() const;
+
+    Board apply_move(const Move &move) const;
+
+    GameResult play_move(const Move &move);
+
+    std::string rating_bar() const;
+    Color get_turn_color() const;
+    Position *get_en_passant() const;
+
+    Board remove_all(const Color &color) const;
+    Board queen_all(const Color &color) const;
+
+    Board set_turn(const Color &color) const;
+
+    int get_material_advantage(const Color &color) const;
+
+    bool is_legal_move(const Move &move, const Color &player_color) const;
+
   private:
     Square squares[64];
     Position *en_passant;
-
-    CastlingRights *black_castling_rights;
-    CastlingRights *white_castling_rights;
 
     Color turn;
 };
@@ -56,6 +102,21 @@ class BoardBuilder {
     BoardBuilder();
     BoardBuilder(const Board &board);
     ~BoardBuilder();
+
+    BoardBuilder row(const Piece &piece);
+    BoardBuilder column(const Piece &piece);
+
+    BoardBuilder piece(Piece *piece);
+    BoardBuilder enable_castling();
+    BoardBuilder disable_castling();
+
+    BoardBuilder enable_queenside_castle(const Color &color);
+    BoardBuilder disable_queenside_castle(const Color &color);
+
+    BoardBuilder enable_kingside_castle(const Color &color);
+    BoardBuilder disable_kingside_castle(const Color &color);
+
+    Board build() const;
 
   private:
     Board *board;
--- src/board.cpp
+++ src/board.cpp
@@ -28,3 +28,223 @@ void CastlingRights::enable_all() {
     enable_kingside();
     enable_queenside();
 }
+
+BoardBuilder::BoardBuilder() {
+    Board *new_board = new Board();
+    new_board->white_castling_rights->disable_all();
+    new_board->black_castling_rights->disable_all();
+    board = new_board;
+}
+
+BoardBuilder::BoardBuilder(const Board &board) {
+    this->board = new Board(board);
+}
+
+BoardBuilder::~BoardBuilder() { delete board; }
+
+BoardBuilder BoardBuilder::row(const Piece &piece) {
+    Position pos = piece.get_pos();
+    while (pos.get_col() > 0) {
+        pos = pos.next_left();
+    }
+
+    for (int i = 0; i < 8; i++) {
+        this->board->get_square(pos) = Square::from_piece(piece.move_to(pos));
+        pos = pos.next_right();
+    }
+
+    return *this;
+}
+
+BoardBuilder BoardBuilder::column(const Piece &piece) {
+    Position pos = piece.get_pos();
+    while (pos.get_row() > 0) {
+        pos = pos.next_below();
+    }
+
+    for (int i = 0; i < 8; i++) {
+        this->board->get_square(pos) = Square::from_piece(piece.move_to(pos));
+        pos = pos.next_above();
+    }
+
+    return *this;
+}
+
+BoardBuilder BoardBuilder::piece(Piece *piece) {
+    Position pos = piece->get_pos();
+    this->board->get_square(pos) = Square::from_piece(piece);
+    return *this;
+}
+
+BoardBuilder BoardBuilder::enable_castling() {
+    this->board->white_castling_rights->enable_all();
+    this->board->black_castling_rights->enable_all();
+    return *this;
+}
+
+BoardBuilder BoardBuilder::disable_castling() {
+    this->board->white_castling_rights->disable_all();
+    this->board->black_castling_rights->disable_all();
+    return *this;
+}
+
+BoardBuilder BoardBuilder::enable_queenside_castle(const Color &color) {
+    switch (color) {
+    case Color::White:
+        this->board->white_castling_rights->enable_queenside();
+        break;
+    case Color::Black:
+        this->board->black_castling_rights->enable_queenside();
+        break;
+    }
+    return *this;
+}
+
+BoardBuilder BoardBuilder::disable_queenside_castle(const Color &color) {
+    switch (color) {
+    case Color::White:
+        this->board->white_castling_rights->disable_queenside();
+        break;
+    case Color::Black:
+        this->board->black_castling_rights->disable_queenside();
+        break;
+    }
+    return *this;
+}
+
+BoardBuilder BoardBuilder::enable_kingside_castle(const Color &color) {
+    switch (color) {
+    case Color::White:
+        this->board->white_castling_rights->enable_kingside();
+        break;
+    case Color::Black:
+        this->board->black_castling_rights->enable_kingside();
+        break;
+    }
+    return *this;
+}
+
+BoardBuilder BoardBuilder::disable_kingside_castle(const Color &color) {
+    switch (color) {
+    case Color::White:
+        this->board->white_castling_rights->disable_kingside();
+        break;
+    case Color::Black:
+        this->board->black_castling_rights->disable_kingside();
+        break;
+    }
+    return *this;
+}
+
+Board BoardBuilder::build() const { return *board; }
+
+Board::Board() {
+    BoardBuilder builder;
+    *this = builder.piece(new Pawn(Color::White, B1))
+                .piece(new Pawn(Color::White, B2))
+                .build();
+    this->en_passant = nullptr;
+}
+
+Board::~Board() {}
+
+double Board::value_for(const Color &ally_color) const {
+    double sum = 0;
+    for (Square square : squares) {
+        if (square.is_empty()) {
+            continue;
+        }
+
+        Piece *piece = square.get_piece();
+        if (piece->get_color() == ally_color) {
+            sum += piece->get_weighted_value();
+        } else {
+            sum -= piece->get_weighted_value();
+        }
+    }
+    return sum;
+}
+
+Color Board::get_current_player_color() const { return turn; }
+
+Square Board::get_square(const Position &pos) const {
+    return this->squares[((7 - pos.get_row()) * 8 + pos.get_col())];
+}
+
+void Board::add_piece(Piece &piece) {
+    Position pos = piece.get_pos();
+    this->get_square(pos) = Square::from_piece(&piece);
+}
+
+Piece *Board::get_piece(const Position &pos) {
+    if (pos.is_off_board()) {
+        return nullptr;
+    }
+    return this->get_square(pos).get_piece();
+}
+
+bool Board::has_ally_piece(const Position &pos, const Color &ally_color) {
+    Piece *piece = this->get_piece(pos);
+    if (piece == nullptr) {
+        return false;
+    }
+    return piece->get_color() == ally_color;
+}
+
+bool Board::has_enemy_piece(const Position &pos, const Color &ally_color) {
+    Piece *piece = this->get_piece(pos);
+    if (piece == nullptr) {
+        return false;
+    }
+    return piece->get_color() == !ally_color;
+}
+
+bool Board::has_piece(const Position &pos) {
+    return this->get_piece(pos) != nullptr;
+}
+
+bool Board::has_no_piece(const Position &pos) {
+    return this->get_piece(pos) == nullptr;
+}
+
+Position Board::get_king_position(const Color &color) const {
+    Position king_pos;
+    for (Square square : this->squares) {
+        Piece *piece = square.get_piece();
+        if (piece == nullptr) {
+            continue;
+        }
+        if (piece->get_type() == Piece::King && piece->get_color() == color) {
+            king_pos = piece->get_pos();
+        }
+    }
+    return king_pos;
+}
+
+bool Board::is_threatened(const Position &pos, const Color &ally_color) const {
+    for (int i = 0; i < 64; i++) {
+        Square square = this->squares[i];
+
+        int row = 7 - (i / 8);
+        int col = i % 8;
+        Position square_pos = Position(row, col);
+
+        if (!square_pos.is_orthogonal_to(pos) &&
+            !square_pos.is_diagonal_to(pos) &&
+            !square_pos.is_knight_move(pos)) {
+            continue;
+        }
+
+        Piece *piece = square.get_piece();
+        if (piece == nullptr) {
+            continue;
+        }
+        if (piece->get_color() == ally_color) {
+            continue;
+        }
+        if (piece->is_legal_attack(pos, *this)) {
+            return true;
+        }
+    }
+    return false;
+}

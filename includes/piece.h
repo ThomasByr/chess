ad11924e025fd2cb6afb6c2c4583d80456f8ebde
Thomas Byr <tbouyer2000@gmail.com>
2022-03-14 15:00:51 +0100

    virtual piece

--- includes/piece.h
+++ includes/piece.h
@@ -4,17 +4,166 @@
 #include <string>
 
 #include "lib.h"
+#include "move.h"
 #include "position.h"
+
+static double WHITE_KING_POSITION_WEIGHTS[8][8] = {
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0},
+    {-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0},
+    {2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0},
+    {2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0},
+};
+
+static double BLACK_KING_POSITION_WEIGHTS[8][8] = {
+    {2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0},
+    {2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0},
+    {-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0},
+    {-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+    {-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0},
+};
+
+static double WHITE_QUEEN_POSITION_WEIGHTS[8][8] = {
+    {-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0},
+    {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0},
+    {-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0},
+    {-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5},
+    {0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5},
+    {-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0},
+    {-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0},
+    {-1.0, -0.0, -1.0, -0.5, -0.5, -0.5, -1.0, -2.0},
+};
+
+static double BLACK_QUEEN_POSITION_WEIGHTS[8][8] = {
+    {-1.0, -0.0, -1.0, -0.5, -0.5, -0.5, -1.0, -2.0},
+    {-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0},
+    {-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0},
+    {0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5},
+    {-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5},
+    {-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0},
+    {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0},
+    {-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0},
+};
+
+static double WHITE_ROOK_POSITION_WEIGHTS[8][8] = {
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0},
+};
+
+static double BLACK_ROOK_POSITION_WEIGHTS[8][8] = {
+    {0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5},
+    {0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5},
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+};
+
+static double WHITE_BISHOP_POSITION_WEIGHTS[8][8] = {
+    {-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0},
+    {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0},
+    {-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0},
+    {-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0},
+    {-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0},
+    {-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0},
+    {-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0},
+    {-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0},
+};
+
+static double BLACK_BISHOP_POSITION_WEIGHTS[8][8] = {
+    {-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0},
+    {-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0},
+    {-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0},
+    {-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0},
+    {-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0},
+    {-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0},
+    {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0},
+    {-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0},
+};
+
+static double WHITE_KNIGHT_POSITION_WEIGHTS[8][8] = {
+    {-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0},
+    {-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0},
+    {-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0},
+    {-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0},
+    {-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0},
+    {-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0},
+    {-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0},
+    {-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0},
+};
+
+static double BLACK_KNIGHT_POSITION_WEIGHTS[8][8] = {
+    {-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0},
+    {-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0},
+    {-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0},
+    {-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0},
+    {-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0},
+    {-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0},
+    {-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0},
+    {-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0},
+};
+
+static double WHITE_PAWN_POSITION_WEIGHTS[8][8] = {
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0},
+    {1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0},
+    {0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5},
+    {0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0},
+    {0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5},
+    {0.5, 1.5, -1.0, -2.0, -2.0, 1.0, 1.5, 0.5},
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+};
+
+static double BLACK_PAWN_POSITION_WEIGHTS[8][8] = {
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+    {0.5, 1.5, -1.0, -2.0, -2.0, 1.0, 1.5, 0.5},
+    {0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5},
+    {0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0},
+    {0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5},
+    {1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0},
+    {5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0},
+    {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+};
 
 class Piece {
   public:
+    inline static const int None = 0;
+    inline static const int King = 1;
+    inline static const int Pawn = 2;
+    inline static const int Knight = 3;
+    inline static const int Bishop = 4;
+    inline static const int Rook = 5;
+    inline static const int Queen = 6;
+
+    inline static const int White = 8;
+    inline static const int Black = 16;
+
+    inline static const int type_mask = 0b00111;
+    inline static const int black_mask = 0b10000;
+    inline static const int white_mask = 0b01000;
+    inline static const int color_mask = black_mask | white_mask;
+
     Piece(Color color, Position position);
-    ~Piece();
+    virtual ~Piece();
 
     Color get_color() const;
     Position get_pos() const;
 
-    void move_to(Position new_pos);
+    int get_type() const;
 
     virtual std::string get_name() const = 0;
     virtual int get_material_value() const = 0;
@@ -24,9 +173,18 @@ class Piece {
     virtual bool is_queenside_rook() const = 0;
     virtual bool is_kingside_rook() const = 0;
 
+    virtual std::vector<Move> get_legal_moves(const Board &board) const = 0;
+    virtual bool is_legal_move(const Position &new_pos,
+                               const Board &board) const = 0;
+    virtual bool is_legal_attack(const Position &new_pos,
+                                 const Board &board) const = 0;
+
+    virtual Piece *move_to(Position new_pos) const = 0;
+
     friend std::ostream &operator<<(std::ostream &os, const Piece &piece);
 
   protected:
+    int id;
     Color color;
     Position position;
 };
@@ -36,6 +194,8 @@ class Pawn : public Piece {
     Pawn(Color color, Position position);
     ~Pawn();
 
+    Pawn *move_to(Position new_pos) const;
+
     std::string get_name() const;
     int get_material_value() const;
     double get_weighted_value() const;
@@ -43,6 +203,10 @@ class Pawn : public Piece {
     bool is_starting_pawn() const;
     bool is_queenside_rook() const;
     bool is_kingside_rook() const;
+
+    std::vector<Move> get_legal_moves(const Board &board) const;
+    bool is_legal_move(const Position &new_pos, const Board &board) const;
+    bool is_legal_attack(const Position &new_pos, const Board &board) const;
 
     friend std::ostream &operator<<(std::ostream &os, const Pawn &pawn);
 };
--- src/piece.cpp
+++ src/piece.cpp
@@ -3,11 +3,81 @@
 Piece::Piece(Color color, Position position) {
     this->color = color;
     this->position = position;
+
+    switch (color) {
+    case Color::White:
+        this->id = Piece::White;
+        break;
+    case Color::Black:
+        this->id = Piece::Black;
+        break;
+    default:
+        this->id = Piece::None;
+        break;
+    }
 }
 
-Piece::~Piece() {}
+// Piece::~Piece() {}
 
 std::ostream &operator<<(std::ostream &os, const Piece &piece) {
+    (void)piece;
     os << "?";
     return os;
 }
+
+Color Piece::get_color() const { return this->color; }
+
+Position Piece::get_pos() const { return this->position; }
+
+int Piece::get_type() const { return this->id & Piece::type_mask; }
+
+Pawn::Pawn(Color color, Position position) : Piece(color, position) {
+    this->id |= Piece::Pawn;
+}
+
+Pawn::~Pawn() {}
+
+std::ostream &operator<<(std::ostream &os, const Pawn &pawn) {
+    switch (pawn.get_color()) {
+    case Color::White:
+        os << "\u2659";
+        break;
+    case Color::Black:
+        os << "\u265F";
+        break;
+    default:
+        os << "?";
+        break;
+    }
+    return os;
+}
+
+Pawn *Pawn::move_to(Position new_pos) const {
+    return new Pawn(this->color, new_pos);
+}
+
+std::string Pawn::get_name() const { return "pawn"; }
+
+int Pawn::get_material_value() const { return 1; }
+
+double Pawn::get_weighted_value() const {
+    double(*weights)[8] = {0};
+    switch (this->color) {
+    case Color::White:
+        weights = WHITE_PAWN_POSITION_WEIGHTS;
+        break;
+    case Color::Black:
+        weights = BLACK_PAWN_POSITION_WEIGHTS;
+        break;
+    }
+    return weights[this->get_pos().get_row()][this->get_pos().get_col()] +
+           (double)this->get_material_value() * 10.;
+}
+
+bool Pawn::is_starting_pawn() const {
+    return this->get_pos().is_starting_pawn(this->get_color());
+}
+
+bool Pawn::is_queenside_rook() const { return false; }
+
+bool Pawn::is_kingside_rook() const { return false; }

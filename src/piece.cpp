798e03a3444d7e06960358a513a589573e01b30f
Thomas Byr <tbouyer2000@gmail.com>
2022-04-01 20:43:50 +0200

    implement moves for bishop

--- src/piece.cpp
+++ src/piece.cpp
@@ -560,3 +560,100 @@ bool Rook::is_legal_attack(const Position &new_pos, Board &board) {
     }
     return false;
 }
+
+Bishop::Bishop(Color color, Position position) : Piece(color, position) {
+    this->id |= Piece::Bishop;
+}
+
+Bishop::~Bishop() {}
+
+Bishop *Bishop::move_to(Position new_pos) const {
+    return new Bishop(this->color, new_pos);
+}
+
+std::string Bishop::get_name() const { return "bishop"; }
+
+int Bishop::get_material_value() const { return 3; }
+
+double Bishop::get_weighted_value() const {
+    double(*weights)[8] = {0};
+    switch (this->color) {
+    case Color::White:
+        weights = WHITE_BISHOP_POSITION_WEIGHTS;
+        break;
+    case Color::Black:
+        weights = BLACK_BISHOP_POSITION_WEIGHTS;
+        break;
+    }
+    return weights[this->get_pos().get_row()][this->get_pos().get_col()] +
+           (double)this->get_material_value() * 10.;
+}
+
+bool Bishop::is_starting_pawn() const { return false; }
+
+bool Bishop::is_queenside_rook() const { return false; }
+
+bool Bishop::is_kingside_rook() const { return false; }
+
+std::vector<Move> Bishop::get_legal_moves(Board &board) {
+    std::vector<Move> result;
+    Color ally_color = this->get_color();
+    Position pos = this->get_pos();
+
+    for (unsigned row = 0; row < 8; row++) {
+        for (unsigned col = 0; col < 8; col++) {
+            Position new_pos = Position(row, col);
+            if (new_pos != pos && !board.has_ally_piece(new_pos, ally_color) &&
+                new_pos.is_diagonal_to(pos)) {
+                Move move;
+                move.move_type = Move::PieceMove;
+                move.from = pos;
+                move.to = new_pos;
+                result.push_back(move);
+            }
+        }
+    }
+    return this->get_valid_moves(result, board);
+}
+
+bool Bishop::is_legal_move(const Position &new_pos, Board &board) {
+    if (board.has_ally_piece(new_pos, this->get_color()) ||
+        new_pos.is_off_board()) {
+        return false;
+    }
+
+    Position pos = this->get_pos();
+    if (pos.is_diagonal_to(new_pos)) {
+        std::vector<Position> traveling = pos.diagonals_to(new_pos);
+        traveling.pop_back();
+
+        for (Position p : traveling) {
+            if (board.has_piece(p)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool Bishop::is_legal_attack(const Position &new_pos, Board &board) {
+    if (board.has_ally_piece(new_pos, this->get_color()) ||
+        new_pos.is_off_board()) {
+        return false;
+    }
+
+    Position pos = this->get_pos();
+    if (pos.is_diagonal_to(new_pos)) {
+        std::vector<Position> traveling = pos.diagonals_to(new_pos);
+        traveling.pop_back();
+
+        for (Position p : traveling) {
+            if (board.has_piece(p)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}

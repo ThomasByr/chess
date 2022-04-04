8fe46e16b50345385547e213e92feba13794b62a
Thomas Byr <tbouyer2000@gmail.com>
2022-04-04 21:32:01 +0200

    src/main : inline doc & clean

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,65 @@
+//! chess-cli
+//!
+//! Copyright (c) 2022, the chess-cli authors.
+//! All rights reserved.
+//!
+//! Redistribution and use in source and binary forms, with or without
+//! modification, are permitted provided that the following conditions are met:
+//!
+//! * Redistributions of source code must retain the above copyright notice,
+//!   this list of conditions and the following disclaimer.
+//!
+//! * Redistributions in binary form must reproduce the above copyright notice,
+//!   this list of conditions and the following disclaimer in the documentation
+//!   and/or other materials provided with the distribution.
+//!
+//! * Neither the name of the chess-cli authors nor the names of its
+//!   contributors may be used to endorse or promote products derived from
+//!   this software without specific prior written permission.
+//!
+//! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+//! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+//! ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+//! LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+//! CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+//! SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+//! INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//! CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//! ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+//! POSSIBILITY OF SUCH DAMAGE.
+//!
+//! @file src/main.cpp
+//! @author ThomasByr <https://github.com/ThomasByr>
+//! @brief Main entry point for the chess-cli executable.
+//!
+//! @see <https://github.com/ThomasByr/chess>
+//!
+//! @param [in] -f, --fen      FEN [default:
+//! "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"]
+//! @param [in] -m, --moves    MOVES [default: ""]
+//! @param [in] -n, --filename FILENAME [default: ""]
+//! @param [in] -v, --verbose
+//! @param [in] -q, --quiet
+//! @param [in] -h, --help
+//! @param [in] -V, --version
+//!
+//! @note -f, -m, -n are mutually exclusive and at the time of writing,
+//!   not yet implemented.
+//!
+//! Move notation is as follows:
+//!  - "e2e4" for a normal move
+//!  - "O-O" or "O-O-O" for a castling move
+//!  - "Nf3" for a pawn promotion (to a knight) (not yet implemented)
+//!  - "best" to let the CPU choose the best move
+//!  - "worst" to let the CPU choose the worst move
+//!  - "rate" to rate the current position
+//!  - "pass" to pass
+//!  - "/quit" to quit the program
+//!
+//! @see <https://github.com/ThomasByr/chess>
+//!
+
 #include <iostream>
 #include <string>
 #include <tuple>
@@ -5,162 +67,11 @@
 
 #include "lib.h"
 
-#include "board.h"
-#include "move.h"
-#include "result.h"
+#include "app.h"
 
-std::string input(std::string prompt) {
-    std::string s;
-    std::cout << prompt;
-    std::cout.flush();
+int main(int argc, char *argv[]) {
+    App app = App(argc, argv);
+    // std::cout << app << std::endl;
 
-    std::getline(std::cin, s);
-    return s;
-}
-
-Move get_cpu_move(Board &board, bool best) {
-    std::tuple<Move, unsigned, double> r;
-    if (best) {
-        r = board.get_next_best_move(4);
-    } else {
-        r = board.get_next_worst_move(4);
-    }
-
-    Move m = std::get<0>(r);
-    unsigned count = std::get<1>(r);
-    std::cout << "CPU evaluated " << count << " moves before choosing to ";
-    std::cout.flush();
-
-    Piece *from;
-    Piece *to;
-    switch (m.move_type()) {
-    case Move::PieceMove:
-        from = board.get_piece(m.from());
-        to = board.get_piece(m.to());
-        if (from != nullptr && to != nullptr) {
-            std::cout << "take " << to->get_name() << "(" << m.to() << ") ";
-            std::cout << "with " << from->get_name() << "(" << m.from() << ")"
-                      << std::endl;
-        } else if (from != nullptr && to == nullptr) {
-            std::cout << "move " << from->get_name() << "(" << m.from() << ") ";
-            std::cout << "to " << m.to() << std::endl;
-        } else {
-            std::cout << "move " << m.from() << " to " << m.to() << std::endl;
-        }
-        break;
-    case Move::KingSideCastle:
-        std::cout << "castle kingside" << std::endl;
-        break;
-    case Move::QueenSideCastle:
-        std::cout << "castle queenside" << std::endl;
-        break;
-    case Move::Resign:
-        std::cout << "resign" << std::endl;
-        break;
-    case Move::Invalid:
-        std::cout << "fliptables" << std::endl;
-        break;
-    }
-    return m;
-}
-
-std::string trim(const std::string &s) {
-    std::string::size_type first = s.find_first_not_of(' ');
-    std::string::size_type last = s.find_last_not_of(' ');
-    return s.substr(first, (last - first + 1));
-}
-
-std::string to_lower(const std::string &s) {
-    std::string r;
-    for (char c : s) {
-        r += tolower(c);
-    }
-    return r;
-}
-
-int main() {
-    Board board = Board::new_board();
-    std::cout << board << std::endl;
-
-    std::vector<Move> history = std::vector<Move>();
-    bool is_running = true;
-
-    while (is_running) {
-        std::string s = input(">>> ");
-        if (!s.empty()) {
-            s = to_lower(trim(s));
-        }
-
-        Move m;
-        if (s.empty() || s == "best" || s == "b") {
-            std::cout << "Waiting for CPU to choose best move..." << std::endl;
-            m = get_cpu_move(board, true);
-        } else if (s == "worst" || s == "w") {
-            std::cout << "Waiting for CPU to choose worst move..." << std::endl;
-            m = get_cpu_move(board, false);
-        } else if (s == "rate" || s == "r") {
-            double score = board.score();
-            std::cout << "Score: " << score << std::endl;
-            continue;
-        } else if (s == "pass" || s == "p") {
-            board = board.change_turn();
-            std::cout << board << std::endl;
-            continue;
-        } else if (s == "/quit") {
-            is_running = false;
-        } else if (s == "history") {
-            for (std::vector<Move>::size_type i = 0; i < history.size(); i++) {
-                if (i < history.size() - 1) {
-                    std::cout << history[i] << " " << history[i + 1]
-                              << std::endl;
-                } else {
-                    std::cout << history[i] << std::endl;
-                }
-            }
-            continue;
-        } else {
-            int t = m.update_from_string(s);
-            switch (t) {
-            case Move::Invalid:
-                std::cerr << "Invalid move: " << s << std::endl;
-                continue;
-                break;
-            }
-        }
-
-        GameResult r;
-        std::cout << "\n";
-        switch ((r = board.play_move(m)).result_type()) {
-        case GameResult::Continuing:
-            board = r.next_board();
-            std::cout << board << std::endl;
-            history.push_back(m);
-            break;
-        case GameResult::Victory:
-            std::cout << r.next_board() << "\n";
-            std::cout << !r.winner() << " loses. " << r.winner()
-                      << " is victorious." << std::endl;
-            history.push_back(m);
-            is_running = false;
-            board = r.next_board();
-            break;
-        case GameResult::IllegalMove:
-            if (is_running) {
-                std::cerr << r.move() << " is an illegal move." << std::endl;
-            }
-            break;
-        case GameResult::Stalemate:
-            std::cout << "Drawn game." << std::endl;
-            history.push_back(m);
-            is_running = false;
-            board = r.next_board();
-            break;
-        }
-    }
-
-    for (Move m : history) {
-        std::cout << m << std::endl;
-    }
-    std::cout << board.end_fen() << std::endl;
-    return 0;
+    return app.run();
 }

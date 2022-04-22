683040a41435702b7b33bd440751ad8ecda919de
Thomas Byr <tbouyer2000@gmail.com>
2022-04-22 17:47:13 +0200

    src/lib : async input [pthread] [doc]

--- src/lib.cpp
+++ src/lib.cpp
@@ -7,6 +7,13 @@ void panic(const std::string &msg) {
     throw std::runtime_error(ss.str());
 }
 
+bool ask(const std::string &msg) {
+    std::string answer;
+    input(answer, msg);
+    answer = to_lower(trim(answer));
+    return answer == "y" || answer == "yes";
+}
+
 Color operator!(Color color) {
     return color == Color::White ? Color::Black : Color::White;
 }
@@ -19,6 +26,47 @@ std::ostream &operator<<(std::ostream &os, Color color) {
         return os << "Black";
     default:
         panic("Invalid color");
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, State state) {
+    switch (state) {
+    case State::CPU_THINKING:
+        return os << "cpu thinking or evaluating";
+    case State::WAITING_FOR_INPUT:
+        return os << "thread waiting for input";
+    case State::PROGRAM_STARTING:
+        return os << "program starting, allocating memory";
+    case State::SORTING_MOVES:
+        return os << "cpu is sorting best moves";
+    case State::MOVING_PIECE:
+        return os << "a piece is moving";
+    case State::CHECKING_CHECK:
+        return os << "checking if the king is in check";
+    case State::CHECKING_CHECKMATE:
+        return os << "checking if the king is in checkmate";
+    case State::CHECKING_STALEMATE:
+        return os << "checking if the game is a stalemate";
+    case State::GETTING_LEGAL_MOVES:
+        return os << "cpu getting all legal moves";
+    case State::PLAYING_MOVES:
+        return os << "cpu playing moves";
+    case State::HANDLING_SIGNAL:
+        return os << "os handling signal";
+    case State::REGISTERING_EXIT:
+        return os << "os registering exit";
+    case State::PARSING_MOVE:
+        return os << "parsing user input move";
+    case State::DISPLAYING_CPU_MOVE:
+        return os << "displaying cpu move as string";
+    case State::PARSING_ARGS:
+        return os << "parsing command line arguments";
+    case State::CHECKING_ARGS:
+        return os << "checking command line arguments";
+    case State::WRITING_OSTREAM:
+        return os << "writing to ostream (long write)";
+    default:
+        panic("Invalid state");
     }
 }
 
@@ -49,6 +97,7 @@ std::string operator*(std::string str, unsigned n) {
 
 void input(std::string &str, const std::string &prompt) {
     // launch a thread to handle input
+    state = State::WAITING_FOR_INPUT;
     std::thread([&str, &prompt]() {
         std::cout << prompt;
         std::cout.flush();

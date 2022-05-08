60fcc255953ef39e6523c52083f89b73892c4d90
ThomasByr <tbouyer2000@gmail.com>
2022-05-08 21:09:41 +0200

    extern

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,8 +1,14 @@
-#include <iostream>
-
 #include "macros.h"
 
+#include "lib.h"
+
+#include "app.h"
+
 unsigned long _no_asserts = 0;
+
+const bool WHITE_IS_FILLED = true;
+State state = State::PROGRAM_STARTING;
+
 
 void dummy_test() {
     assert_eq(1, 1);
